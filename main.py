9f5ffc3e40ddb62c7da8688bbd4b6cf971f0cc80
shubham-pyc <shubhamg2404@gmail.com>
2022-11-17 02:11:08 -0700

    -Updated bugs for not found

--- main.py
+++ main.py
@@ -18,10 +18,13 @@ def make_list_url(name):
 
 def get_anime_list(name):
     list_url = make_list_url(name)
+    print(list_url)
     page = requests.get(list_url)
-
+    print(page)
+    ret_value = []
     if page:
         soup = BeautifulSoup(page.content, 'html.parser')
+        print(page)
         episode_li = soup.select("#episode_related .name")
         ret_value = []
         for li in episode_li:
@@ -29,9 +32,12 @@ def get_anime_list(name):
             episode = re.search(r'\d+', text)
             if episode:
                 ret_value.append(episode.group())
+            else:
+                print("here")
 
     else:
         print("Error while getting page list: ", page)
+        print("Invalid anime")
 
     return ret_value
 
@@ -41,11 +47,19 @@ def get_anime_url(name, episode):
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     link = soup.select("#playerframe")
-    link = link[0].get('src')
-    if "embed" in link:
-        return BASE_URL+link
+    if len(link):
+        link = link[0].get('src')
+        if "embed" in link:
+            return BASE_URL+link
+        else:
+            return "https:" + link
     else:
-        return "https:" + link
+        not_found = soup.select(".content_left h1")
+        if len(not_found):
+            print("Anime not found")
+            return not_found[0]
+        else:
+            return "Unknown"
     # return "https:" + str(link[0].get('src'))
 
 
@@ -66,17 +80,19 @@ def main():
     # print(get_path())
     name = args.anime
     episode = args.episode
-
     
     if episode <  0:
         episode_list = get_anime_list(name)
-        episode = episode_list[-1]
+        if len(episode_list):
+            episode = episode_list[-1]
+
+
+    print(get_anime_url(name, episode))
 
     # episode_list = get_anime_list(name)
 
     # latest = episode_list[-1]
 
-    print(get_anime_url(name, episode))
 
 
 
